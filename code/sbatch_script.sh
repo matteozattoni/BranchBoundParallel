a35fe7363a8e7232f8b87f324ab3cd971e5d5342
matteozattoni <matteo.zattoni@icloud.com>
2023-08-25 22:05:23 +0200

    Create sbatch_script.sh

---
+++ code/sbatch_script.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+# Job name:
+#SBATCH --job-name=bra_bound
+#
+# Partition:
+#SBATCH --partition=broadwell
+#
+# Number of nodes needed for use case:
+#SBATCH --nodes=4
+#
+# Wall clock limit 4 hours:
+#SBATCH --time=04:00:00
+#
+# Log
+#SBATCH -o %j.log
+#
+# Error
+#SBATCH -e %j.err
+
+## Command(s) to run (example):
+eval spack load --sh openmpi@4.1.3%gcc@9.4.0
+srun /bin/hostname ## print the hostname
+srun --mpi=pmix ./branchbound.out ##pmix is the slurm launcher (like mpirun) 
